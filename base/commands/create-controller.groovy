ab13be08bf575337b2d3f34b5edd8b1e2170258c
Zachary Klein <zak@silver-chalice.com>
2018-04-27 15:22:46 -0500

    Added lang flag, autodetect language for controller

--- base/commands/create-controller.groovy
+++ base/commands/create-controller.groovy
@@ -2,26 +2,39 @@ description("Creates a controller and associated test") {
     usage "mn create-controller [CONTROLLER NAME]"
     argument name: 'Controller Name', description: "The name of the controller to create", required: true
     flag name: 'force', description: "Whether to overwrite existing files"
-    flag name: 'groovy', description: "Create a Groovy controller"
-    flag name: 'kotlin', description: "Create a Kotlin controller"
+    flag name: 'lang', description: "The language used for the controller (options: groovy, kotlin, java)"
 }
 
 def model = model(args[0]).forConvention("Controller")
-boolean overwrite = flag('force')
-String artifactPath = "${model.packagePath}/${model.className}"
 def controllerDestination, controllerTemplate, testDestination, testTemplate
+def warning = { l -> "Warning: no ${l} source directory - it will be created"}
 
-if (flag('groovy')) {
+String lang = flag('lang')
+String artifactPath = "${model.packagePath}/${model.className}"
+
+boolean overwrite = flag('force')
+boolean groovySrc = new File('src/main/groovy').exists()
+boolean kotlinSrc = new File('src/main/kotlin').exists()
+boolean javaSrc = new File('src/main/java').exists()
+
+
+if (lang == 'groovy' || (!lang && groovySrc)) {
+    if(!groovySrc) println warning('Groovy')
+
     controllerTemplate = template('groovy/Controller.groovy')
     controllerDestination = file("src/main/groovy/${artifactPath}.groovy")
     testTemplate = template('groovy/ControllerSpec.groovy')
     testDestination = file("src/test/groovy/${artifactPath}Spec.groovy")
-} else if (flag('kotlin')) {
+} else if (lang == 'kotlin' || (!lang && kotlinSrc)) {
+    if(!kotlinSrc) println warning('Kotlin')
+
     controllerTemplate = template('kotlin/Controller.kt')
     controllerDestination = file("src/main/kotlin/${artifactPath}.kt")
     testTemplate = template('kotlin/ControllerTest.kt')
     testDestination = file("src/test/kotlin/${artifactPath}Test.kt")
-} else {
+} else if (lang == 'java' || (!lang && javaSrc)) {
+    if(!javaSrc) println warning('Java')
+
     controllerTemplate = template('java/Controller.java')
     controllerDestination = file("src/main/java/${artifactPath}.java")
     testTemplate = template('java/ControllerTest.java')
