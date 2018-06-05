b9282ceaacb2dcd514ab0251aa7963636b2ae185
Zachary Klein <zak@silver-chalice.com>
2018-06-05 07:05:37 -0200

    Added @CompileStatic

--- base/features/groovy/skeleton/src/main/groovy/@defaultPackage.path@/Application.groovy
+++ base/features/groovy/skeleton/src/main/groovy/@defaultPackage.path@/Application.groovy
@@ -1,9 +1,11 @@
 package @defaultPackage@
 
 import io.micronaut.runtime.Micronaut
+import groovy.transform.CompileStatic
 
+@CompileStatic
 class Application {
     static void main(String[] args) {
-        Micronaut.run(getClass())
+        Micronaut.run(Application.class)
     }
 }
