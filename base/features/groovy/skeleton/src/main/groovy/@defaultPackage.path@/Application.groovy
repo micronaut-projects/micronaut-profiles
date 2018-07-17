b856997987010d2ab762b67834cdf35333d0c6ee
jameskleeh <james.kleeh@gmail.com>
2018-07-17 13:54:33 -0400

    Minor groovy feature change

--- base/features/groovy/skeleton/src/main/groovy/@defaultPackage.path@/Application.groovy
+++ base/features/groovy/skeleton/src/main/groovy/@defaultPackage.path@/Application.groovy
@@ -6,6 +6,6 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class Application {
     static void main(String[] args) {
-        Micronaut.run(Application.class)
+        Micronaut.run(Application)
     }
 }
