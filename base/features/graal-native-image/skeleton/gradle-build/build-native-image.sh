12b2c2dc2a23bf854e356318bb593c211648136c
Graeme Rocher <graeme.rocher@gmail.com>
2018-10-05 10:59:05 +0200

    Updates for GraalVM rc7

--- base/features/graal-native-image/skeleton/gradle-build/build-native-image.sh
+++ base/features/graal-native-image/skeleton/gradle-build/build-native-image.sh
@@ -8,4 +8,5 @@ native-image --class-path build/libs/@app.name@-0.1-all.jar \
 			 -H:Class=@defaultPackage@.Application \
 			 -H:+ReportUnsupportedElementsAtRuntime \
 			 -H:+AllowVMInspection \
-			 --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder
+			 --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
+			 --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom
--- base/features/graal-native-image/skeleton/maven-build/build-native-image.sh
+++ base/features/graal-native-image/skeleton/maven-build/build-native-image.sh
@@ -8,4 +8,5 @@ native-image --class-path target/@app.name@-0.1.jar \
 			 -H:Class=@defaultPackage@.Application \
 			 -H:+ReportUnsupportedElementsAtRuntime \
 			 -H:+AllowVMInspection \
-			 --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder
+			 --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
+			 --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom
