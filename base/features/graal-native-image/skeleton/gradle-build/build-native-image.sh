a8b4ad587ee5f0459331fa7e5065e9db16f2c95a
Iván López <lopezi@objectcomputing.com>
2018-10-24 13:26:01 +0200

    Bring back "build-native-image.sh" script to create the Graal native image without using Docker

---
+++ base/features/graal-native-image/skeleton/gradle-build/build-native-image.sh
@@ -0,0 +1,13 @@
+./gradlew assemble
+java -cp build/libs/@app.name@-0.1-all.jar io.micronaut.graal.reflect.GraalClassLoadingAnalyzer
+native-image --no-server \
+             --class-path build/libs/@app.name@-0.1-all.jar \
+             -H:ReflectionConfigurationFiles=build/reflect.json \
+             -H:EnableURLProtocols=http \
+             -H:IncludeResources="logback.xml|application.yml|META-INF/services/*.*" \
+             -H:Name=@app.name@ \
+             -H:Class=@defaultPackage@.Application \
+             -H:+ReportUnsupportedElementsAtRuntime \
+             -H:+AllowVMInspection \
+             --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
+             --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom
---
+++ base/features/graal-native-image/skeleton/maven-build/build-native-image.sh
@@ -0,0 +1,13 @@
+./mvnw package
+java -cp target/@app.name@-0.1.jar io.micronaut.graal.reflect.GraalClassLoadingAnalyzer
+native-image --no-server \
+             --class-path target/@app.name@-0.1.jar \
+             -H:ReflectionConfigurationFiles=target/reflect.json \
+             -H:EnableURLProtocols=http \
+             -H:IncludeResources="logback.xml|application.yml|META-INF/services/*.*" \
+             -H:Name=@app.name@ \
+             -H:Class=@defaultPackage@.Application \
+             -H:+ReportUnsupportedElementsAtRuntime \
+             -H:+AllowVMInspection \
+             --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
+             --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom
