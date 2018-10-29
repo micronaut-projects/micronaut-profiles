8cb2bbaaa1c91aaac5490add3671933f903810df
Iván López <lopezi@objectcomputing.com>
2018-10-29 15:03:37 +0100

    Add support for Graal-rc8

--- base/features/graal-native-image/skeleton/maven-build/build-native-image.sh
+++ base/features/graal-native-image/skeleton/maven-build/build-native-image.sh
@@ -9,5 +9,6 @@ native-image --no-server \
              -H:Class=@defaultPackage@.Application \
              -H:+ReportUnsupportedElementsAtRuntime \
              -H:+AllowVMInspection \
+             -H:-UseServiceLoaderFeature \
              --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
              --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom
