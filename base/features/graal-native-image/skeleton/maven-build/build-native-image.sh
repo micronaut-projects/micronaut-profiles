de52ecc7cffe4fb2c45b40ed9a46539eddf8acae
Iván López <lopezi@objectcomputing.com>
2019-01-21 17:23:24 +0100

    Update Docker Graal

--- base/features/graal-native-image/skeleton/maven-build/build-native-image.sh
+++ base/features/graal-native-image/skeleton/maven-build/build-native-image.sh
@@ -10,5 +10,6 @@ native-image --no-server \
              -H:+ReportUnsupportedElementsAtRuntime \
              -H:+AllowVMInspection \
              -H:-UseServiceLoaderFeature \
+             --allow-incomplete-classpath \
              --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
              --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom,com.sun.jndi.dns.DnsClient
