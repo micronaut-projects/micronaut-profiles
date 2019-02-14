330a446141986c3bc9392fae1c3ff49f413bfa21
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2019-02-14 14:16:58 +0200

    Using the same build-native-image.sh inside Dockerfile

--- base/features/aws-api-gateway-graal/skeleton/gradle-build/build-native-image.sh
+++ base/features/aws-api-gateway-graal/skeleton/gradle-build/build-native-image.sh
@@ -1,8 +1,9 @@
-./gradlew assemble
-java -cp build/libs/@app.name@-*.jar io.micronaut.graal.reflect.GraalClassLoadingAnalyzer
-native-image --no-server \
+#!/bin/sh
+GRAALVM_HOME=${GRAALVM_HOME:-/usr/lib/graalvm}
+${GRAALVM_HOME}/bin/java -cp build/libs/@app.name@-*.jar io.micronaut.graal.reflect.GraalClassLoadingAnalyzer
+${GRAALVM_HOME}/bin/native-image --no-server \
              --class-path build/libs/@app.name@-*.jar \
-             -H:ReflectionConfigurationFiles=src/main/resources/reflect.json,build/reflect.json \
+             -H:ReflectionConfigurationFiles=src/main/resources/reflect.json,src/main/resources/netty-reflect.json,build/reflect.json \
              -H:EnableURLProtocols=http \
              -H:IncludeResources="logback.xml|application.yml|META-INF/services/*.*" \
              -H:Name=server \
@@ -13,3 +14,4 @@ native-image --no-server \
              --allow-incomplete-classpath \
              --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
              --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom,com.sun.jndi.dns.DnsClient,io.micronaut.function.aws.proxy.MicronautLambdaContainerHandler,com.amazonaws.serverless.proxy.internal.LambdaContainerHandler,io.netty.handler.ssl.JdkNpnApplicationProtocolNegotiator,io.netty.handler.ssl.ReferenceCountedOpenSslEngine
+
