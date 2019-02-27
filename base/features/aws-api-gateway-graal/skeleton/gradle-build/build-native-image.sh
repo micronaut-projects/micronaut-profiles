b858b02d3614f22d506cc72e1d9e57236ee868ad
graemerocher <graeme.rocher@gmail.com>
2019-02-27 14:03:17 +0100

    Use service loader / fix maven scripts

--- base/features/aws-api-gateway-graal/skeleton/gradle-build/build-native-image.sh
+++ base/features/aws-api-gateway-graal/skeleton/gradle-build/build-native-image.sh
@@ -5,12 +5,11 @@ ${GRAALVM_HOME}/bin/native-image --no-server \
              --class-path build/libs/@app.name@-*.jar \
              -H:ReflectionConfigurationFiles=src/main/resources/reflect.json,src/main/resources/netty-reflect.json,build/reflect.json \
              -H:EnableURLProtocols=http \
-             -H:IncludeResources="logback.xml|application.yml|META-INF/services/*.*" \
+             -H:IncludeResources="logback.xml|application.yml" \
              -H:Name=server \
              -H:Class=io.micronaut.function.aws.runtime.MicronautLambdaRuntime \
              -H:+ReportUnsupportedElementsAtRuntime \
              -H:-AllowVMInspection \
-             -H:-UseServiceLoaderFeature \
              --allow-incomplete-classpath \
              --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
              --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom,com.sun.jndi.dns.DnsClient,io.micronaut.function.aws.proxy.MicronautLambdaContainerHandler,com.amazonaws.serverless.proxy.internal.LambdaContainerHandler,io.netty.handler.ssl.JdkNpnApplicationProtocolNegotiator,io.netty.handler.ssl.ReferenceCountedOpenSslEngine
