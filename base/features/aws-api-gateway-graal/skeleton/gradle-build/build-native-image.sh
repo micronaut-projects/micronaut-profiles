54eda08f117500129b85726c7bd57c0645ba01fd
graemerocher <graeme.rocher@gmail.com>
2019-03-15 10:57:50 +0100

    Remove redundant flags for build native image scripts

--- base/features/aws-api-gateway-graal/skeleton/gradle-build/build-native-image.sh
+++ base/features/aws-api-gateway-graal/skeleton/gradle-build/build-native-image.sh
@@ -2,14 +2,8 @@
 GRAALVM_HOME=${GRAALVM_HOME:-/usr/lib/graalvm}
 ${GRAALVM_HOME}/bin/native-image --no-server \
              --class-path build/libs/@app.name@-*.jar \
-             -H:ReflectionConfigurationFiles=build/classes/@sourceLanguage@/main/META-INF/reflect.json \
-             -H:EnableURLProtocols=http \
              -H:IncludeResources="logback.xml|application.yml" \
              -H:Name=server \
-             -H:Class=io.micronaut.function.aws.runtime.MicronautLambdaRuntime \
-             -H:+ReportUnsupportedElementsAtRuntime \
-             -H:-AllowVMInspection \
-             --allow-incomplete-classpath \
-             --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
-             --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom,com.sun.jndi.dns.DnsClient,io.micronaut.function.aws.proxy.MicronautLambdaContainerHandler,com.amazonaws.serverless.proxy.internal.LambdaContainerHandler,io.netty.handler.ssl.JdkNpnApplicationProtocolNegotiator,io.netty.handler.ssl.ReferenceCountedOpenSslEngine
+             -H:Class=io.micronaut.function.aws.runtime.MicronautLambdaRuntime 
+             -H:-AllowVMInspection 
 
--- base/features/graal-native-image/skeleton/gradle-build/build-native-image.sh
+++ base/features/graal-native-image/skeleton/gradle-build/build-native-image.sh
@@ -1,13 +1,7 @@
 ./gradlew assemble
 native-image --no-server \
              --class-path build/libs/@app.name@-*.jar \
-             -H:ReflectionConfigurationFiles=build/classes/@sourceLanguage@/main/META-INF/reflect.json \
-             -H:EnableURLProtocols=http \
              -H:IncludeResources="logback.xml|application.yml" \
              -H:Name=@app.name@ \
              -H:Class=@defaultPackage@.Application \
-             -H:+ReportUnsupportedElementsAtRuntime \
-             -H:+AllowVMInspection \
-             --allow-incomplete-classpath \
-             --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
-             --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom,com.sun.jndi.dns.DnsClient
+             -H:+AllowVMInspection 
