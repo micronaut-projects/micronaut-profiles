706ddd52f960785da4fb8824fef474ba23b53a38
Iván López <lopezi@objectcomputing.com>
2019-04-25 10:44:42 +0200

    Map the port instead using --network because that only works in Linux

--- base/features/graal-native-image/skeleton/docker-build.sh
+++ base/features/graal-native-image/skeleton/docker-build.sh
@@ -3,4 +3,4 @@ docker build . -t @app.name@
 echo
 echo
 echo "To run the docker container execute:"
-echo "    $ docker run --network host @app.name@"
+echo "    $ docker run -p 8080:8080 @app.name@"
